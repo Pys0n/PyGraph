f6ba7aec5c0044b2bfb3fbc08e47b3359ce8f102
Pys0n <jason.krueger2010@web.de>
2025-09-03 07:49:59 +0200

    added vertical bar charts: VBarChart()-class

--- widgets/charts.py
+++ widgets/charts.py
@@ -1,98 +1,235 @@
-class Direction:
-    VERTICAL = 0
-    HORIZONTAL = 1
+from widgets.text import ColoredBackground
 
 
-class Position:
-    X = 0
-    Y = 1
+BarColor = ColoredBackground
 
 
-class Axis:
-    def __init__(self, name: str = '', pos: Position = Position.X, labels: list[str] = []):
+class Chart:
+    def __init__(self, name: str, x_axis: list, y_axis: list, values: dict) -> None:
+        if not isinstance(name, str):
+            raise TypeError('Excpected name to be a str, got '+type(name).__name__)
+        if not isinstance(x_axis, list):
+            raise TypeError('Excpected x_axis to be a list, got '+type(x_axis).__name__)
+        if not isinstance(y_axis, list):
+            raise TypeError('Excpected y_axis to be a list, got '+type(y_axis).__name__)
+        if not isinstance(values, dict):
+            raise TypeError('Excpected values to be a dict, got '+type(values).__name__)
+        
         self.name = name
-        self.pos = pos      # "x" or "y"
-        self.labels = labels
-        self.shown_labels = []
-
-
-    def set_labels(self, labels: list[str]) -> None:
-        '''
-        Sets the labels
-        '''
-        self.labels = labels
-
-
-    def set_position(self, pos: Position) -> None:
-        '''
-        Sets the axis position ("x" or "y")
-        '''
-        self.pos = pos
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+        self.values = values
 
 
     def set_name(self, name: str) -> None:
         '''
-        Sets the name of the axis
+        Sets the name of the chart
         '''
+        if not isinstance(name, str):
+            raise TypeError('Excpected name to be a str, got '+type(name).__name__)
+        
         self.name = name
 
 
-    def config_labels(self, shown_labels: list[str]) -> None:
-        self.shown_labels = shown_labels
+    def set_x_axis(self, x_axis: list) -> None:
+        '''
+        Sets the x axis of the chart
+        '''
+        if not isinstance(x_axis, list):
+            raise TypeError('Excpected x_axis to be a list, got '+type(x_axis).__name__)
+        
+        self.x_axis = x_axis
 
 
-class BarChart:
-    def __init__(self, title: str = '', *, x: Axis = None, y: Axis = None, direction: Direction = Direction.VERTICAL, values: list[tuple[str, str]] = []) -> None:
-        self.title = title
-        self.xaxis = x
-        self.yaxis = y
-        self.direction = direction
+    def set_y_axis(self, y_axis: list) -> None:
+        '''
+        Sets the y axis of the chart
+        '''
+        if not isinstance(y_axis, list):
+            raise TypeError('Excpected y_axis to be a list, got '+type(y_axis).__name__)
+        
+        self.y_axis = y_axis
+
+
+    def set_values(self, values: dict) -> None:
+        '''
+        Sets the values of the chart
+        '''
+        if not isinstance(values, dict):
+            raise TypeError('Excpected values to be a dict, got '+type(values).__name__)
+        
         self.values = values
 
 
-    def set_axis(self, xy: Position, axis: Axis) -> None:
-        if xy == Position.X:
-            self.xaxis = axis
-        elif xy == Position.Y:
-            self.yaxis = axis
+    def get_name(self) -> str:
+        '''
+        Returns the name of the chart
+        '''
+        return self.name
+
+
+    def get_x_axis(self) -> list:
+        '''
+        Returns the x axis of the chart
+        '''
+        return self.x_axis
+
+
+    def get_y_axis(self) -> list:
+        '''
+        Returns the y axis of the chart
+        '''
+        return self.y_axis
     
 
-    def set_title(self, title: str):
+    def get_values(self) -> dict:
         '''
-        Sets the title of the bar chart
+        Returns the values of the chart
         '''
-        self.title = title
-
-
-    def set_direction(self, direction: Direction) -> None:
-        '''
-        Sets the direction of the bar chart
-        '''
-        self.direction = direction
+        return self.values
     
 
-    def set_values(self, values: list[tuple[str, str]]) -> None:
-        '''
-        Sets the values of the bar chart
-        '''
-        self.values = values
 
+class BarChart(Chart):
+    def __init__(self, name: str, x_axis: list, y_axis: list, bar_width: int = 1, *, values: dict = {}) -> None:
+        super().__init__(name, x_axis, y_axis, values)
+        self.bar_width: int = bar_width
+    
+
+    def add_bar(self, x, y) -> None:
+        '''
+        Adds a bar that starts at x and goes up to y
+
+        x and y must be items of x_axis and y_axis
+        '''
+        if x not in self.x_axis:
+            raise ValueError('The charts x axis has no item named "'+str(x)+'"')
+        if y not in self.y_axis:
+            raise ValueError('The charts y axis has no item named "'+str(y)+'"')
+        
+        self.values[x] = [y, BarColor.WHITE]
+
+
+    def delete_bar(self, x) -> None:
+        '''
+        Deletes the bar at the position x
+
+        x must be a item of x_axis
+        '''
+        if x not in self.x_axis:
+            raise ValueError('The charts x axis has no item named "'+str(x)+'"')
+        if x not in self.values.keys():
+            raise ValueError('There is no bar at "'+str(x)+'"')
+        
+        self.values[x][0] = None
+
+
+    def set_all_bar_colors(self, bar_color: str) -> None:
+        '''
+        Sets the color of all bars
+        '''
+        if not isinstance(bar_color, str):
+            raise TypeError('Excpected bar_color to be a str, got '+type(bar_color).__name__)
+
+        for key in self.values:
+            self.values[key][1] = bar_color
+
+    
+    def set_bar_color(self, x, bar_color: str) -> None:
+        '''
+        Sets the color of the bar at position x
+        '''
+        if x not in self.x_axis:
+            raise ValueError('The charts x axis has no item named "'+str(x)+'"')
+        if x not in self.values.keys():
+            raise ValueError('There is no bar at "'+str(x)+'"')
+        if not isinstance(bar_color, str):
+            raise TypeError('Excpected bar_color to be a str, got '+type(bar_color).__name__)
+
+        self.values[x][1] = bar_color
+
+
+    def set_bar_width(self, width: int) -> None:
+        '''
+        Sets the width of all bars
+        '''
+        if not isinstance(width, int):
+            raise TypeError('Excpected width to be an int, got '+type(width).__name__)
+        
+        self.bar_width = width
+
+
+    def get_bar_width(self) -> int:
+        '''
+        Returns the width of all bars
+        '''
+        return self.bar_width
+    
 
     def print(self) -> None:
         '''
-        Prints the bar chart
+        Prints the graph to the terminal
         '''
-        print('X:', self.xaxis.labels)
-        print('Y:', self.yaxis.labels)
-        print('Values:\n', self.values)
+        print(str(self))
+              
 
-        if self.direction == Direction.VERTICAL:
-            print('                 Diagramm              ')
-            print('      ^                                ')
-            print('T     │                                ')
-            print('E  DE ┼##################              ')
-            print('S     │                                ')
-            print('T  US ┼######                          ')
-            print('      │                                ')
-            print('      └─────+─────+─────+─────+─────>  ')
-            print('            1     2     3     4        ')
+    def get_str(self) -> str:
+        '''
+        Returns the graph as string
+        '''
+        return str(self)
+
+
+    def __str__(self) -> str:
+        graph = []
+
+        self.y_axis.reverse()
+        y_axis = self.y_axis.copy()
+        self.y_axis.reverse()
+
+        space_before_graph = 0
+        for item in y_axis:
+            if len(str(item)) > space_before_graph:
+                space_before_graph = len(str(item)) + 2
+
+        graph.append(' ' * space_before_graph + '  │\n')
+        print_bars = set()
+        for item in y_axis:
+            line = ' ' + str(item).rjust(space_before_graph) + ' ┼ '
+
+            for key in self.values:
+                if str(self.values[key][0]) == str(item) or key in print_bars:
+                    print_bars.add(key)
+                    line += str('#'*self.bar_width).center((len(str(key))//2 if len(str(key))//2 > self.bar_width*4-2 else 0)+(self.bar_width*4))
+                else:
+                    line += str(' '*self.bar_width).center((len(str(key))//2 if len(str(key))//2 > self.bar_width*4-2 else 0)+(self.bar_width*4))
+
+            
+
+            graph.append(line+'\n')
+            for _ in range(self.bar_width):
+                line = ' ' * space_before_graph + '  │ '
+                for key in self.values:
+                    if str(self.values[key][0]) == str(item) or key in print_bars:
+                        print_bars.add(key)
+                        line += str('#'*self.bar_width).center((len(str(key))//2 if len(str(key))//2 > self.bar_width*4-2 else 0)+(self.bar_width*4))
+                    else:
+                        line += str(' '*self.bar_width).center((len(str(key))//2 if len(str(key))//2 > self.bar_width*4-2 else 0)+(self.bar_width*4))
+                graph.append(line + '\n')
+
+        x = ' ' * (space_before_graph+2) + '└─'
+
+        for item in self.x_axis:
+            item_len = len(str(item))//2
+            x += '┼'.center((item_len if item_len > self.bar_width*4-2 else 0)+(self.bar_width*4), '─')
+
+        graph.append(x + '\n')
+
+        x_text = ' ' * (space_before_graph+4)
+
+        for item in self.x_axis:
+            x_text += str(item).center((len(str(item))//2 if len(str(item))//2 > self.bar_width*4-2 else 0)+(self.bar_width*4), ' ')
+
+        graph.append(x_text + '\n')
+
+        return ''.join(graph)
